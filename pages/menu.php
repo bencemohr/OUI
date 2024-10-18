4fb293494585888d57dd109abe8b0ece14b56edc
osinavhe Akhigbe <osinavhe.akhigbe@student.nhlstenden.com>
2024-10-18 10:23:05 +0000

    made the menu-dessert page

---
+++ pages/menu.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Team IT1F">
+    <title>OUI</title>
+    <link rel="stylesheet" href="../styles/menu-desserts.css" type="text/css">
+</head>
+<body>
+    <main>
+        <h1><b>DESSERT</b><img src="../src/img/Croissant.svg " alt="logo"></h1>
+       
+        <div class="container">
+            <div class="containe">
+                <?php
+                    $file = fopen("../menuchange.txt", "r");
+
+                    while(! feof($file))
+                    {
+                        $line = fgets($file);
+                        echo $line. "<br>";
+                    }
+                ?>
+            </div>
+
+            <div class="contain">
+                <?php
+                    $file = fopen("../Macarons.txt", "r");
+
+                    while(! feof($file))
+                    {
+                        $line = fgets($file);
+                        echo $line. "<br>";
+                    }
+                ?>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
