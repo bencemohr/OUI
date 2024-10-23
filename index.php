8b56b4ef631dbceea2a7662b063bfd8d316f3c73
Sara Kiani Nejad <sara.kiani.nejad@student.nhlstenden.com>
2024-10-23 19:16:59 +0330

    Update the design and add the header

--- index.php
+++ index.php
@@ -10,31 +10,50 @@
 <body>
    
     <main>
-    
-      <div class="container">
+      <div class="logo">
+        <a href="file:///C:/University/Web%20Development/NHL_Stenden_PHP_Docker_Env-0.4/app/public/Oui/OUI/index.html#" target="_self"><img src="src/img/general/logo.svg"></a>
+      </div>
       
-       <div class="container1">
-        <svg width="859" height="842" viewBox="0 0 859 842" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path opacity="0.4" d="M125.037 440.489C125.037 440.489 342.747 274.382 435.024 353.011C520.111 425.513 402.455 626.027 490.161 695.699C573.124 761.603 684.708 730.215 740.784 672.078C787.936 623.193 768.68 498.533 716.061 419.203C640.604 305.442 539.999 380.861 468.914 281.924C410.383 200.46 354.478 146.595 367.201 48.9404C391.875 -140.442 730.668 295.097 837 255.385M168.997 841C21.5735 810.791 39.8254 646.836 128.402 618.461C196.016 596.802 250.633 704.466 316.421 727.213C389.172 752.368 470.254 772.527 483.074 741.311" stroke="#EFD9C1" stroke-opacity="0.4" stroke-linecap="round"/>
-        <path opacity="0.4" d="M887 343.924C769.87 377.633 396.673 7.94246 369.493 168.692C355.478 251.582 417.06 297.303 481.535 366.451C559.838 450.429 670.66 386.413 753.78 482.975C811.742 550.31 832.953 656.123 781.013 697.618C719.243 746.964 596.327 773.607 504.94 717.668C408.326 658.529 537.931 488.331 444.204 426.791C324.56 348.234 103.778 500.481 1 555.925M151.162 841C-11.2334 815.358 8.87202 676.192 106.444 652.107C180.924 633.723 241.087 725.108 313.556 744.417C393.694 765.768 483.011 782.879 497.132 756.383" stroke="#EFD9C1" stroke-opacity="0.4" stroke-linecap="round"/>
-        </svg>
-
-       </div>
-       
-       <div class="container2">
-         
-       </div>
+      <header>
         
-       <div class="container3">
-         <img src="src/img/general/Image.png">
+        <nav>
+          
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Welcome</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Our story</a></li>
+            <li><a href="#">Review</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+
+        </nav>
+
+        <div class="social-media">
+          <a href="#"><img src="src/img/general/facebook.svg"></a>
+          <a href="#"><img src="src/img/general/instagram.svg"></a>
+        </div>
+      </header>
+      <hr>
+      <div class="container">
+
+        <div class="container1">
+          <img src="src/img/general/Image.png" alt="Image" id="img">
         </div>
 
-       <div class="container4">
-         <h2>Overpriced coffee got you dragging?</h2>
-         <p>It might be time to reconsider where you're spending your caffeine euros.</p>
-       </div>
+        <div class="container2">
+          <h2>Overpriced coffee got you dragging?</h2>
+          <p>It might be time to reconsider where you're spending your caffeine euros.</p>
+        </div>
 
-       </div>
+        <div class="container3">
+            <?php
+
+            ?>
+        </div>
+        
+      </div>
+      
     </main>
 
         
