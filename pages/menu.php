385daa8c5e0c319a1a76dceef377fed29d3e7f99
Ai Nguyen <ai.nguyen@student.nhlstenden.com>
2024-10-23 17:32:14 +0200

    Add header with logo and media

--- pages/menu.php
+++ pages/menu.php
@@ -7,46 +7,69 @@
     <link rel="stylesheet" href="../style.css" type="text/css">
 </head>
 <body>
-    <div class="container">
-        <div class="menu">
-            <h1>MENU</h1>
+    
+    <main>
+        <div class="container">
+            <header>
+                <img src="../src/img/general/logo.svg" alt="logo">
+                    <nav>
+                        <ul>
+                            <?php
+                                $menuNav= array("HOME","WELCOME","MENU","OUR STORY","REVIEW","CONTACT");
+                                $link= array("","","","","","");
+                                for ($i=0; $i < count($menuNav); $i++) {
+                                    echo "<li><a href='".$link[$i]."'>".$menuNav[$i]."</a></li>";
+                                }
+                            ?>
+                        </ul>
+                    </nav>
+                    <div class="socialMedia">
+                        <img src="../src/img/general/facebook.svg" alt="facebook">
+                        <img src="../src/img/general/instagram.svg" alt="instagram">
+                </div>
+                </header>
+                
+                <div class="menu">
+                    <h1>MENU</h1>
+                </div>
+            <div class="drinks">
+                <h1>DRINKS</h1>
+                <div class="coffee">
+                    <div class="icon">
+                        <h2>COFFEE</h2>
+                        <img src="../src/img/general/coffee.svg" alt="coffee">
+                    </div>
+                    <div class="coffeeItem">
+                        <?php
+                            $coffeeFile= fopen("../src/menu-coffee.txt","r");
+                            while (! feof($coffeeFile)) {
+                                $coffeeLine = fgets($coffeeFile);
+                                echo "<p>" . $coffeeLine . "</p>";
+                            }
+                            fclose($coffeeFile);
+                        
+                        ?>
+                    </div>
+                </div>   
+                <div class="cdrink">
+                    <div class="icon">
+                        <h2>COLD DRINKS & TEA</h2>
+                        <img src="../src/img/general/cold drinks.svg" alt="Cold">
+                    </div>
+                    <div class="cdrinkItem">
+                        <?php
+                            $cdrinkFile= fopen("../src/menu-cdrink.txt","r");
+                            while (! feof($cdrinkFile)) {
+                                $cdrinkLine = fgets($cdrinkFile);
+                                echo "<p>" . $cdrinkLine . "</p>";
+                            }
+                            fclose($cdrinkFile);
+                        ?>
+                    </div>
+                </div>         
+            </div>
         </div>
-        
-        <div class="drinks">
-            <h1>DRINKS</h1>
-            <div class="coffee">
-                <div class="icon">
-                    <h2>COFFEE</h2>
-                    <img src="../src/img/general/coffee.svg" alt="coffee">
-                </div>
-                <div class="coffeeItem">
-                    <?php
-                        $coffeeFile= fopen("../src/menu-coffee.txt","r");
-                        while (! feof($coffeeFile)) {
-                            $coffeeLine = fgets($coffeeFile);
-                            echo "<p>" . $coffeeLine . "</p>";
-                        }
-                        fclose($coffeeFile);
-                    ?>
-                </div>
-            </div>   
-            <div class="cdrink">
-                <div class="icon">
-                    <h2>COLD DRINKS & TEA</h2>
-                    <img src="../src/img/general/cold drinks.svg" alt="Cold">
-                </div>
-                <div class="cdrinkItem">
-                    <?php
-                        $cdrinkFile= fopen("../src/menu-cdrink.txt","r");
-                        while (! feof($cdrinkFile)) {
-                            $cdrinkLine = fgets($cdrinkFile);
-                            echo "<p>" . $cdrinkLine . "</p>";
-                        }
-                        fclose($cdrinkFile);
-                    ?>
-                </div>
-            </div>         
-        </div>
-    </div>
+            
+    </main>
 </body>
 </html> 
