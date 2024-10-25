34d07c58c53e5e25fe16de2add8bef6130ebe5e7
andreaskonstantinou <andreas.konstantinou@student.nhlstenden.com>
2024-10-25 09:39:08 +0200

    indexpage

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="./styles/index.css" type="text/css">
+  </head>
+  <body>
+    <div class="container">
+  
+
+    </div>
+
+    <div class="containter-2">
+      <div class="Text">
+        <h2>Start your <?php echo date("l"); ?> with a cup<Br>
+        of coffee at OUI</h2>
+      </div>
+      <div class="TextParagraph">
+        <p>
+          Whether the weather is warm or <br />
+          cold you can enjoy the coffee in hot <br />
+          and cold brew.
+        </p>
+      </div>
+      
+      <div class="button-container">
+          <a href="#" class="menu-button">MENU</a>
+          
+      </div>
+    </div>
+
+   
+    
+   
+  </body>
+</html>
+    
